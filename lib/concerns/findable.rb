8aa2492e61c3ae775d2d9d8168f343e3be2b182c
Felice Forby <feliceforby@gmail.com>
2018-05-14 22:36:58 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,5 @@
 module Findable
   def find_by_name(name)
-    all.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
-  
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-  
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
 end
