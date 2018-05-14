6fa505ace217502e6f4101ff8146ea9fe2c70da8
Felice Forby <feliceforby@gmail.com>
2018-05-14 22:46:12 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,10 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
-
-  def count
-    self.all.count
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+  
+    def count
+      self.all.count
+    end
 end
