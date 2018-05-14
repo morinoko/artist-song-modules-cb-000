e36f14303670b9ae07fc871f33a90933a3153114
Felice Forby <feliceforby@gmail.com>
2018-05-14 22:49:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,17 +11,9 @@ class Song
 
   @@songs = []
 
-  def initialize
-    # @@songs << self
-  end
-
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 
   def self.all
     @@songs
